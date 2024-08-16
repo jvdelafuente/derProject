13ff65e4f97b35332ea0babcb0dad5b6bb81d778
Javierdelafuente <jv94delafuente@gmail.com>
2024-08-16 12:32:20 +0200

    vite.config delete

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://derproject-production.up.railway.app', // Ajusta el puerto según sea necesario
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
+
 })
